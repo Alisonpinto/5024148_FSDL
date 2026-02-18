75c0412bd206882608cbb2c57575eb18c3725016
Alison Pinto <145547550+Alisonpinto@users.noreply.github.com>
2026-02-18 06:14:10 +0000

    week 4

---
+++ week04_18022026/first.js
@@ -0,0 +1,19 @@
+// test1.js
+
+// VARIABLES
+let name = "Alison";
+let age = 20;
+
+// OPERATORS
+let yearOfBirth = 2025 - age;
+
+// PRINTING OUTPUT
+console.log("Hello my name is " + name);
+console.log("I was born in " + yearOfBirth);
+
+// CONDITIONALS
+if (age >= 18) {
+    console.log("I am an adult.");
+} else {
+    console.log("I am a minor.");
+}
